41ca9379e0af3c3a0a396220a41e8a3fce6ef3ab
Maarten Claes <maartencls@gmail.com>
2014-04-17 23:01:37 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,30 @@
+
+C8 = ./node_modules/.bin/component
+SRC = $(wildcard lib/*/*.js)
+APP = /tmp/loginator.nw
+
+run: build
+	@rm -f $(APP)
+	zip -rq $(APP) *
+ifeq ($(shell uname), Linux)
+	`which nw` $(APP)
+else
+	open -n -a node-webkit $(APP)
+endif
+
+build: node_modules components $(SRC)
+	@$(C8) build --standalone loginator
+
+components:
+	@$(C8) install
+
+node_modules: package.json
+	@npm install
+
+clean:
+	rm -fr build
+
+distclean: clean
+	rm -fr components
+
+.PHONY: clean distclean run
---
+++ index.js
@@ -0,0 +1,35 @@
+var win = gui.Window.get();
+
+var require = node;
+var ansispan = require('ansispan');
+var spawn = require('child_process').spawn;
+var fs = require('fs');
+
+// Use custom colors
+ansispan.foregroundColors = {
+  '30': '#F56E75', // black
+  '31': '#BE3532', // red
+  '32': '#8A9824', // green
+  '33': '#A88820', // yellow
+  '34': '#268bd2', // blue
+  '35': '#6D6CD3', // purple
+  '36': '#96A0A0', // default: cyan
+  '37': '#F7F5E3'
+};
+
+window.openFile = function() {
+
+  var path = document.querySelector('.filepath').getAttribute('value');
+  var tail = spawn("tail", ["-f", path]);
+  var logArea = document.querySelector('.js-log-area');
+
+  var codeify = function(data) {
+    var stringified = String(data);
+    stringified = stringified.split("\n").join("</p><p class='code'>");
+    return "<p class='code'>" + stringified + "</p>";
+  };
+
+  tail.stdout.on("data", function (data) {
+    logArea.innerHTML = logArea.innerHTML + ansispan(codeify(data));
+  });
+};
