a6802593a82d121d656420c9013c3a79e85a14c9
Maarten Claes <maartencls@gmail.com>
2014-04-19 11:22:46 +0200

    Added LogFormatter

--- index.js
+++ index.js
@@ -2,27 +2,9 @@ var win = gui.Window.get();
 
 var require = node;
 
-var ansispan = require('ansispan');
 var fs = require('fs');
-var LogTracker = require('log-tracker');
-
-// Use custom colors
-ansispan.foregroundColors = {
-  '30': '#F56E75', // black
-  '31': '#BE3532', // red
-  '32': '#8A9824', // green
-  '33': '#A88820', // yellow
-  '34': '#268bd2', // blue
-  '35': '#6D6CD3', // purple
-  '36': '#96A0A0', // default: cyan
-  '37': '#F7F5E3'
-};
-
-var codeify = function(data) {
-  var stringified = String(data);
-  stringified = stringified.split("\n").join("</p><p class='code'>");
-  return "<p class='code'>" + stringified + "</p>";
-};
+var LogTracker    = require('log-tracker');
+var LogFormatter  = require('log-formatter');
 
 window.openFile = function() {
   var body    = document.querySelector('body');
@@ -32,7 +14,8 @@ window.openFile = function() {
   var logPath = document.querySelector('.filepath').getAttribute('value');
   var logTracker = new LogTracker(logPath);
   logTracker.on("data", function (data) {
-    logArea.innerHTML = logArea.innerHTML + ansispan(codeify(data));
+    var html = new LogFormatter(data).toHtml();
+    logArea.innerHTML = logArea.innerHTML + html;
     body.scrollTop = body.scrollHeight;
   });
 };
