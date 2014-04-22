32326cdc63c450e1288124148c38946a3ea53bfb
Maarten Claes <maartencls@gmail.com>
2014-04-22 12:41:58 +0200

    Extract trackLog()

--- index.js
+++ index.js
@@ -6,17 +6,21 @@ var fs = require('fs');
 var LogTracker    = require('log-tracker');
 var LogFormatter  = require('log-formatter');
 
-window.openFile = function() {
-  var body    = document.querySelector('body');
+var trackLog = function(filepath) {
+  var logTracker = new LogTracker(filepath);
+  var body = document.querySelector('body');
   var logArea = document.querySelector('.js-log-area');
   logArea.innerHTML = '';
 
-  var logFile = document.querySelector('.filepath').files[0];
-  if (!logFile) { alert('Please select a log file.'); }
-  var logTracker = new LogTracker(logFile.path);
   logTracker.on("data", function (data) {
     var html = new LogFormatter(data).toHtml();
     logArea.innerHTML = logArea.innerHTML + html;
     body.scrollTop = body.scrollHeight;
   });
+};
+
+window.openFile = function() {
+  var logFile = document.querySelector('.filepath').files[0];
+  if (!logFile) { alert('Please select a log file.'); }
+  trackLog(logFile.path);
 };
