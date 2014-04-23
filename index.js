ba1b5655ffd453d23f5cc8a76aa85a8d8a07ad45
Maarten Claes <maartencls@gmail.com>
2014-04-23 20:01:06 +0200

    Some more experimenting with IndexedDb

--- index.js
+++ index.js
@@ -6,7 +6,45 @@ var fs = require('fs');
 var LogTracker    = require('log-tracker');
 var LogFormatter  = require('log-formatter');
 
+var database = require('database');
+
+var fetchLogs = function(onSuccess) {
+  var logs = [];
+  var store = database.db.transaction("log_files").objectStore("log_files");
+  store.openCursor().onsuccess = function(event) {
+    var cursor = event.target.result;
+    if (cursor) {
+      logs.push(cursor.value);
+      cursor.continue();
+    } else {
+      onSuccess(logs);
+    }
+  };
+};
+
+database.onDbReady = function() {
+  var logs = fetchLogs(function(logs) {
+    console.log("All stored logs:", logs);
+  });
+};
+
+
+var storeLogFile = function(filepath) {
+  var transaction = database.db.transaction(["log_files"], "readwrite");
+  var store = transaction.objectStore("log_files");
+
+  var request = store.add({path: filepath});
+  request.onsuccess = function(event) {
+    console.log("Saved log: " + filepath);
+  };
+
+  transaction.onerror = function(event) {
+    alert("Couldn't store log file path ...");
+  };
+};
+
 var trackLog = function(filepath) {
+  storeLogFile(filepath);
   var logTracker = new LogTracker(filepath);
   var body = document.querySelector('body');
   var logArea = document.querySelector('.js-log-area');
@@ -21,6 +59,6 @@ var trackLog = function(filepath) {
 
 window.openFile = function() {
   var logFile = document.querySelector('.filepath').files[0];
-  if (!logFile) { alert('Please select a log file.'); }
+  if (!logFile) { return alert('Please select a log file.'); }
   trackLog(logFile.path);
 };
